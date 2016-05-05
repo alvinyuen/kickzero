1992a78789ae8dc34b2c4069758fdcedf7225d35
ruan <ruanshuzhi@gtan.com>
2016-05-05 18:57:47 +0800

    grunt

---
+++ GruntFile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8082,
+                    base: './deploy'
+                }
+            }
+        },
+        concat: {
+            dist: {
+                src: [  "src/lib/**/*.js", "src/game/**/*.js"],
+                dest: 'deploy/js/<%= pkg.name %>-v<%=pkg.version%>.js'
+            }
+        },
+        watch: {
+            files: 'src/**/*.js',
+            tasks: ['concat']
+        },
+        open: {
+            dev: {
+                path: 'http://localhost:8082/index.html'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['concat', 'connect', 'open', 'watch']);
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## TODO
+find sprites or gg
+
